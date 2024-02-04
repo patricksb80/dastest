50151822657e2e20ec2d7dc5b3a14abb951d5ee3
patricksb80 <71671996+patricksb80@users.noreply.github.com>
2024-02-04 13:09:45 -0300

    Create main.py
    Dev Lab

---
+++ main.py
@@ -0,0 +1,20 @@
+#sentences = ['s', 'sim']
+#isSure = input('É estudante? (s/n):\n').lower().strip() == 's'
+#  print(isSure)
+
+sentences = ['s', 'sim']
+isSure = input('É estudante? (s/n):\n') in sentences
+teste = isSure
+if isSure in sentences:
+  print(teste)
+
+#if nom in lista:
+ 
+#nom = str(input('Você gosta de mim? '))
+
+#lista = ['sim','claro','obvio']
+
+#if nom in lista:
+#  print('obrigado, bom saber')
+#else:
+#  print('Que pena')
